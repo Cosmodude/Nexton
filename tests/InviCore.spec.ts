6340c0b508e5874584025cb7b9acd21641a8d86d
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-24 16:20:10 +0900

    developed tests

--- tests/InviCore.spec.ts
+++ tests/InviCore.spec.ts
@@ -1,18 +1,20 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { Address, toNano } from 'ton-core';
 import { Invicore } from '../wrappers/InviCore';
 import '@ton-community/test-utils';
-let deployer
+import { randomAddress } from '@ton-community/test-utils';
+let deployer: SandboxContract<TreasuryContract>
 
 describe('InviCore', () => {
     let blockchain: Blockchain;
     let invicore: SandboxContract<Invicore>;
     let myAddress: Address = new Address(0, Buffer.from("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu"));
+    const nftContract: Address= new Address(0, Buffer.from("EQAMLDBglwTu1QwgCZPXCqTKdi7Uro4wVvydHZqx-tvGf0DT"));
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury('deployer');
-
-        invicore = blockchain.openContract(await Invicore.fromInit(deployer.address,deployer.address,deployer.address));
+        
+        invicore = blockchain.openContract(await Invicore.fromInit(nftContract, myAddress));
         
         const deployResult = await invicore.send(
             deployer.getSender(),
@@ -31,6 +33,7 @@ describe('InviCore', () => {
             deploy: true,
             success: true,
         });
+
         const owner = await invicore.getOwner();
         expect(owner.equals( deployer.address)).toBe(true);
         console.log("Owner: ", owner)
@@ -40,4 +43,42 @@ describe('InviCore', () => {
         // the check is done inside beforeEach
         // blockchain and invicore are ready to use
     });
+
+    it('should change Staking Pool address', async() => {
+        console.log("Changing Address!!!")
+        const user = await blockchain.treasury('user');
+        const addressBefore = await invicore.getNftContract();
+        console.log("Address before: ", addressBefore);
+
+        await invicore.send(
+            user.getSender(), 
+            {
+            value: toNano("0.2")
+            }, 
+            {   
+                $$type: 'ChangeAddr',
+                address: randomAddress(),
+                entity: "nft"
+            }
+        )
+
+        let addressAfter = await invicore.getNftContract();
+        console.log("Address after (user): ", addressAfter);
+
+        await invicore.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.2")
+            }, 
+            {   
+                $$type: 'ChangeAddr',
+                address: randomAddress(),
+                entity: "nft"
+            }
+        )
+        
+        addressAfter = await invicore.getNftContract();
+        console.log("Address after (deployer)): ", addressAfter);
+
+    })
 });
