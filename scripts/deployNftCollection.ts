b9925b5a5994f8359c5cc0161ff347a879272cd5
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-08 12:47:53 +0700

    trying to add off chain data

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -3,14 +3,13 @@ import { NftCollection, buildNftCollectionContentCell } from '../wrappers/NftCol
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const randomSeed= Math.floor(Math.random() * 10000)
     const nftCollection = provider.open(NftCollection.createFromConfig({
         ownerAddress: provider.sender().address as Address,
         nextItemIndex: 0,
         collectionContent: buildNftCollectionContentCell(
             {
-                collectionContent: "https://github.com/ton-blockchain/token-contract/blob/main/nft/web-example/my_collection.json",
-                commonContent: "https://github.com/ton-blockchain/token-contract/blob/main/nft/web-example/my_collection.json"
+                collectionContent: "https://github.com/Cosmodude/Invincible_LS/blob/main/sampleMetadata.json",
+                commonContent: ""
             }
         ),
         nftItemCode: await compile("NftItem"),
