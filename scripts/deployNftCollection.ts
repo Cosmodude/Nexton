45d886d208ca43770ef4f02ce9bc21e11d5da6e6
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-14 12:31:00 +0700

    first onchain attempt

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -1,17 +1,16 @@
-import { Address, beginCell, toNano } from 'ton-core';
+import { Address, Dictionary, beginCell, toNano } from 'ton-core';
 import { NftCollection, buildNftCollectionContentCell, NftCollectionContent } from '../wrappers/NftCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
 
 export async function run(provider: NetworkProvider) {
+    let collectionContent = Dictionary.empty(Dictionary.Keys.Buffer(32));
+    collectionContent.set(Buffer.from("name"), Buffer.from("NexTon"));
     const nftCollection = provider.open(NftCollection.createFromConfig({
         ownerAddress: myAddress, 
         nextItemIndex: 0,
-        collectionContent: buildNftCollectionContentCell({
-            collectionContent: 'https://raw.githubusercontent.com/Cosmodude/Invincible_LS/main/sampleMetadata.json',  // collection metadata
-            commonContent: 'https://raw.githubusercontent.com/Cosmodude/Invincible_LS/main/sampleMetadata.json'     // for nft items 
-        }),
+        collectionContent: beginCell().storeDict(collectionContent).endCell(),
         nftItemCode: await compile("NftItem"),
         royaltyParams: {
             royaltyFactor: 5,
