cc50c3142e717159068c8d36c69881d937ef1d20
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-14 22:01:53 +0700

    trying to encode the keys

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -2,11 +2,20 @@ import { Address, Dictionary, beginCell, toNano } from 'ton-core';
 import { NftCollection, buildNftCollectionContentCell, NftCollectionContent } from '../wrappers/NftCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
+
 let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
 
+const sha256 = (message: string) => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    return data;
+};
+
 export async function run(provider: NetworkProvider) {
-    let collectionContent = Dictionary.empty(Dictionary.Keys.Buffer(32));
-    collectionContent.set(Buffer.from("name"), Buffer.from("NexTon"));
+    let collectionContent = Dictionary.empty(Dictionary.Keys.Uint(8), Dictionary.Values.Buffer(32));
+    collectionContent.set(sha256("name"), Buffer.from("NexTon User"));
+    collectionContent.set(Buffer.from("description"), Buffer.from("NFts proving users' deposits"));
+    console.log(collectionContent)
     const nftCollection = provider.open(NftCollection.createFromConfig({
         ownerAddress: myAddress, 
         nextItemIndex: 0,
