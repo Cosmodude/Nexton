60ddecf801907ac4b4fc33aef08ba7c2499d9c87
Cosmodude <vladislav.lenskii@gmail.com>
2023-08-22 20:06:06 +0900

    simplified the deployment script

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -1,37 +1,19 @@
-import { Address, Dictionary, beginCell, toNano, Cell } from 'ton-core';
-import { NftCollection, buildNftCollectionContentCell, NftCollectionContent } from '../wrappers/NftCollection';
+import { Address, toNano } from 'ton-core';
+import { NftCollection } from '../wrappers/NftCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
-import { sha256_sync } from 'ton-crypto'
+import { buildNftCollectionContentCell } from '../wrappers/collectionContent/offChain';
 
 let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
 
-function toSha256(s: string): bigint {
-    return BigInt('0x' + sha256_sync(s).toString('hex'))
-}
-
-function toTextCell(s: string): Cell {
-    return beginCell().storeUint(0, 8).storeStringTail(s).endCell()
-}
-
-const collectionContentDict = Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell())
-    .set(toSha256("name"), toTextCell("Collection name"))
-    .set(toSha256("description"), toTextCell("Collection description"))
-    .set(toSha256("image"), toTextCell("https://hipo.finance/hton.png"));
-
-// const commonContentDict = Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell())
-//     .set(toSha256("name"), toTextCell("NexTon User"))
-//     .set(toSha256("description"), toTextCell("Nft proving user's deposit"))
-//     .set(toSha256("image"), toTextCell("https://s.getgems.io/nft/b/c/62fba50217c3fe3cbaad9e7f/image.png"));
-
-const content = beginCell().storeUint(0,8).storeDict(collectionContentDict).endCell()
-
-console.log(content)
 
 export async function run(provider: NetworkProvider) {
     const nftCollection = provider.open(NftCollection.createFromConfig({
         ownerAddress: myAddress, 
         nextItemIndex: 0,
-        collectionContent: content,
+        collectionContent: buildNftCollectionContentCell({
+            collectionContent: "https://raw.githubusercontent.com/Cosmodude/Invincible_LS/main/sampleMetadata.json",
+            commonContent:""
+        }),
         nftItemCode: await compile("NftItem"),
         royaltyParams: {
             royaltyFactor: 9,
