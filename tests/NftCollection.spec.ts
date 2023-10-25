0d541bd6ef099ad5f51f3be49a6f81353cd764b4
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-25 11:27:03 +0900

    slight updates on scripts and tests

--- tests/NftCollection.spec.ts
+++ tests/NftCollection.spec.ts
@@ -34,8 +34,8 @@ describe('NftCollection', () => {
             ownerAddress: deployer.address,
             nextItemIndex: 0,
             collectionContent: buildCollectionContentCell({
-                name: "Collection name",
-                description: "Collection description",
+                name: "Nexton",
+                description: "Nexton description",
                 image: "https://hipo.finance/hton.png"
             }),
             nftItemCode: await compile("NftItem"),
@@ -63,65 +63,69 @@ describe('NftCollection', () => {
         // blockchain and nftCollection are ready to use
     });
 
-    // it('should set and read Item metadata properly', async () => {
+    it('should set and read Item metadata properly', async () => {
 
-    //     deployer = await blockchain.treasury('deployer');
-    //     nexton = await blockchain.treasury('nexton');
+        deployer = await blockchain.treasury('deployer');
+        nexton = await blockchain.treasury('nexton');
 
-    //     let collectionData = await nftCollection.getCollectionData();
-    //     expect(collectionData.ownerAddress).toEqualAddress(deployer.address);
+        let collectionData = await nftCollection.getCollectionData();
+        expect(collectionData.ownerAddress).toEqualAddress(deployer.address);
 
-    //     const mint = await nftCollection.sendMintNft(deployer.getSender(), {
-    //         value: toNano("0.6"),
-    //         amount: toNano("0.5"),
-    //         itemIndex: 0,
-    //         itemOwnerAddress: randomAddress(),
-    //         nextonAddress: nexton.address,
-    //         itemContent: setItemContentCell({
-    //             name: "Item name",
-    //             description: "Item description",
-    //             image: "https://hipo.finance/hton.png"
-    //         }),
-    //         queryId: Date.now()
-    //     })
-    //     //console.log("EVENT: ", mint.events);
+        const mint = await nftCollection.sendMintNft(deployer.getSender(), {
+            value: toNano("0.6"),
+            amount: toNano("0.5"),
+            itemIndex: 0,
+            itemOwnerAddress: randomAddress(),
+            nextonAddress: nexton.address,
+            itemContent: setItemContentCell({
+                name: "Item name",
+                description: "Holds information about the user's stake in the Nexton platform pool",
+                image: "https://raw.githubusercontent.com/Cosmodude/TAP/main/TAP_Logo.png",
+                principal: 10n,
+                leverageRatio: 1n,
+                lockPeriod: 600n,
+                lockEnd: 10n
+            }),
+            queryId: Date.now()
+        })
+        console.log("EVENT: ", mint.transactions);
 
         
 
-    //     const index: TupleItemInt = {
-    //         type: "int",
-    //         value: 0n
-    //     }
+        const index: TupleItemInt = {
+            type: "int",
+            value: 0n
+        }
        
-    //     const itemAddress = await nftCollection.getItemAddressByIndex(index);
-    //     //console.log("Address", itemAddress);
+        // const itemAddress = await nftCollection.getItemAddressByIndex(index);
+        // //console.log("Address", itemAddress);
 
-    //     expect(mint.transactions).toHaveTransaction({
-    //         from: nftCollection.address,
-    //         to: itemAddress,
-    //         deploy: true,
-    //         success: true,
-    //     });
+        // expect(mint.transactions).toHaveTransaction({
+        //     from: nftCollection.address,
+        //     to: itemAddress,
+        //     deploy: true,
+        //     success: true,
+        // });
 
-    //     nftItem = blockchain.openContract(NftItem.createFromAddress(itemAddress));
-    //     expect(nftItem.address).toEqualAddress(itemAddress);
-    //     const itemData = await nftItem.getItemData();
-    //     expect(itemData.collectionAddress).toEqualAddress(nftCollection.address);
-    //     expect(itemData.nextonAddress).toEqualAddress(nexton.address);
+        // nftItem = blockchain.openContract(NftItem.createFromAddress(itemAddress));
+        // expect(nftItem.address).toEqualAddress(itemAddress);
+        // const itemData = await nftItem.getItemData();
+        // expect(itemData.collectionAddress).toEqualAddress(nftCollection.address);
+        // expect(itemData.nextonAddress).toEqualAddress(nexton.address);
 
-    //     //console.log(itemData.itemContent);
-    //     const cs = itemData.itemContent.beginParse();
-    //     const tag = cs.loadUint(8);
-    //     //console.log("Tag", tag)
-    //     expect(tag).toEqual(0);
-    //     const dict = cs.loadDict(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell());
-    //     const nameCS = dict.get(toSha256("name"))?.beginParse()!!;
-    //     //console.log(nameCS);
-    //     await nameCS.loadUint(8);
-    //     const name = await nameCS?.loadStringTail();
-    //     //console.log(name);
-    //     expect(name).toMatch("Item name");
-    // });
+        // //console.log(itemData.itemContent);
+        // const cs = itemData.itemContent.beginParse();
+        // const tag = cs.loadUint(8);
+        // //console.log("Tag", tag)
+        // expect(tag).toEqual(0);
+        // const dict = cs.loadDict(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell());
+        // const nameCS = dict.get(toSha256("name"))?.beginParse()!!;
+        // //console.log(nameCS);
+        // await nameCS.loadUint(8);
+        // const name = await nameCS?.loadStringTail();
+        // //console.log(name);
+        // expect(name).toMatch("Item name");
+    });
 
     // it('should send items to nexton freely (withdraw)', async() => {
     //     deployer = await blockchain.treasury('deployer');
