5f1af2ff6519f947e334afaacb9cd9fc96df41e5
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-25 15:49:46 +0900

    scripts to get item data

---
+++ scripts/GetItemData.ts
@@ -0,0 +1,26 @@
+import { Address, toNano, Dictionary} from 'ton-core';
+import { NftItem } from '../wrappers/NftItem';
+import { NetworkProvider, sleep } from '@ton-community/blueprint';
+import { toSha256 } from '../scripts/collectionContent/onChain';
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const ui = provider.ui();
+
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Item address'));
+    
+
+    const nftItem = provider.open(NftItem.createFromAddress(address));
+
+    const data = await nftItem.getItemData()
+    console.log(data);
+
+    const contentS = data.itemContent.beginParse();
+    const prefix = contentS.loadUint(8)
+    console.log("Prefix: ", prefix )
+    //const contDict = data.itemContent.get(toSha256("name"))?.beginParse();
+    //const contDict = contentS.loadDict(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell());
+    //const nameS = await contDict.get(toSha256("name"))?.beginParse();
+    //const prefix1 = nameS?.loadUint(8);
+    console.log("Prefix: ", contentS);
+    //console.log(nameS?.loadStringTail());
+}
