c36bd71a21190094c0ea7db0fa81fdf4de82a065
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-25 18:40:11 +0900

    scripts to get data about meridians

--- scripts/exploreMeridian.ts
+++ scripts/exploreMeridian.ts
@@ -11,13 +11,17 @@ import {
 } from 'tonapi-sdk-js';
 import axios from 'axios';
 import { load } from 'ts-dotenv';
+import { Address } from 'ton-core';
 
 const env = load({
     TONAPI_KEY: String
 });
+
 const TONAPI_URL = "https://tonapi.io/";
+const TONAPI_TESTNET_URL= "https://testnet.tonapi.io/";
 const address = "kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu";
-const collection = "EQCB47QNaFJ_Rok3GpoPjf98cKuYY1kQwgqeqdOyYJFrywUK";
+const MERIDIAN_COLLECTION = "EQAVGhk_3rUA3ypZAZ1SkVGZIaDt7UdvwA4jsSGRKRo-MRDN";
+const WHALES_COLLECTION = "EQDvRFMYLdxmvY3Tk-cfWMLqDnXF_EclO2Fp4wwj33WhlNFT";
 
 async function fetchData() {
     // Token should be issued on https://tonconsole.com
@@ -31,23 +35,50 @@ async function fetchData() {
 
 }
 
-async function fetchDataWithHeaders() {
+async function fetchCollection(collectionAddress: string) {
+    let data: any;
     try {
       const response = await axios.get(
-        TONAPI_URL + "/" ,
+        TONAPI_URL + "/v2/nfts/collections/" + collectionAddress,
         {
             headers: {
                 Authorization: 'Bearer ' + env.TONAPI_KEY,
             }
         }
       )
-      const data = response.data;
+      data = response.data;
       console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    
+}
+
+//fetchCollection(MERIDIAN_COLLECTION)
+//fetchCollection(WHALES_COLLECTION)
+
+async function fetchItems(collectionAddress: string) {
+    let data: any;
+    try {
+      const response = await axios.get(
+        TONAPI_URL + "/v2/nfts/collections/" + collectionAddress + "/items",
+        {   
+            params: {
+                limit: 10
+            },
+            headers: {
+                Authorization: 'Bearer ' + env.TONAPI_KEY,
+            }
+        }
+      )
+      data = response.data;
+      //console.log(data);
+      return data
     } catch (error) {
       console.error('Error fetching data:', error);
     }
 }
 
-
+fetchItems(MERIDIAN_COLLECTION).then(data => {console.log(data.nft_items[0].metadata)})
   
   
