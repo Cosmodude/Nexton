8517b84d490b45fee28cec50b5e63c9ab92dd8bb
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-26 13:13:42 +0900

    minting to collection deploy script

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -1,10 +1,13 @@
 import { Address, toNano } from 'ton-core';
 import { NftCollection } from '../wrappers/NftCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
-import { buildCollectionContentCell } from './collectionContent/onChain';
+import { buildCollectionContentCell, setItemContentCell } from './collectionContent/onChain';
 
 let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
 
+const randomSeed= Math.floor(Math.random() * 10000);
+
+const nextonAddress = Address.parse("EQBDqObEyc8KYOuHCKm0evBNp0hJ9derp8eSIdhYMjIeMRSZ");
 
 export async function run(provider: NetworkProvider) {
     const nftCollection = provider.open(NftCollection.createFromConfig({
@@ -32,4 +35,23 @@ export async function run(provider: NetworkProvider) {
     await nftCollection.sendDeploy(provider.sender(), toNano('0.05'));
     console.log()
     await provider.waitForDeploy(nftCollection.address);
+
+    const mint = await nftCollection.sendMintNft(provider.sender(),{
+        value: toNano("0.04"),
+        queryId: randomSeed,
+        amount: toNano("0.025"),
+        itemIndex: 0,
+        itemOwnerAddress: myAddress,
+        nextonAddress: nextonAddress,
+        itemContent: setItemContentCell({
+            name: "Nexton Staking Derivative",
+            description: "Holds information about the user's stake in the Nexton platform pool",
+            image: "https://raw.githubusercontent.com/Cosmodude/TAP/main/TAP_Logo.png",
+            principal: 10n,
+            leverageRatio: 1n,
+            lockPeriod: 600n,
+            lockEnd: 10n
+        })
+    })
+    console.log(`NFT Item deployed at https://testnet.tonscan.org/address/${nftCollection.address}`);
 }
