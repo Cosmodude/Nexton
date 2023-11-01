d368ac484ccf2daad412710dfd052ca466170430
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-01 15:44:19 +0900

    updated changeowner script

--- scripts/changeCollectionOwner.ts
+++ scripts/changeCollectionOwner.ts
@@ -2,22 +2,21 @@ import { Address, toNano } from 'ton-core';
 import { NftCollection } from '../wrappers/NftCollection';
 import { NetworkProvider, sleep } from '@ton-community/blueprint';
 
-const nextonAddress: Address = Address.parse("EQAgm4FTha6Aj0GYRmfcmRVBvaVd-smYYkd3Jjy86-50ndq7");
 
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Collection address'));
-    
+    const collectionAddress = Address.parse(args.length > 0 ? args[0] : await ui.input('Collection address'));
+    const nextonAddress = Address.parse(args.length > 0 ? args[0] : await ui.input('New owner address'));
 
-    const nftCollection = provider.open(NftCollection.createFromAddress(address));
+    const nftCollection = provider.open(NftCollection.createFromAddress(collectionAddress));
 
     const tx = await nftCollection.sendChangeOwner(provider.sender(),{
         value: toNano("0.02"),
         newOwnerAddress: nextonAddress,
         queryId: BigInt(Date.now())
     })
-    ui.write('Collection owner changed deployed');
+    ui.write('Collection owner changed');
 
     //ui.write();
     
