8232214f487c3851931e490e10a388494164df22
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-08 15:03:43 +0900

    error with config

--- scripts/deployBasicNominatorPool.ts
+++ scripts/deployBasicNominatorPool.ts
@@ -1,9 +1,39 @@
-import { toNano } from 'ton-core';
+import { toNano, Address, Dictionary } from 'ton-core';
 import { BasicNominatorPool } from '../wrappers/BasicNominatorPool';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { randomAddress } from '@ton-community/test-utils';
 
 export async function run(provider: NetworkProvider) {
-    const basicNominatorPool = provider.open(BasicNominatorPool.createFromConfig({}, await compile('BasicNominatorPool')));
+
+    let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
+
+    const randomSeed= Math.floor(Math.random() * 10000);
+
+    const nextonAddress = Address.parse("EQBDqObEyc8KYOuHCKm0evBNp0hJ9derp8eSIdhYMjIeMRSZ");
+
+    const basicNominatorPool = provider.open(BasicNominatorPool.createFromConfig(
+        {
+            state: 0,
+            nominators_count: 0,
+            stake_amount_sent: 0,
+            validator_amount: 0,
+            config: {
+                validator_address: randomAddress(),
+                validator_reward_share: 10,
+                max_nominator_count: 2, 
+                min_validator_stake: 100,
+                min_Nominator_stake: 100
+            },
+            nominators: Dictionary.empty(Dictionary.Keys.BigUint(256),Dictionary.Values.Cell() ),
+            withdraw_requests: Dictionary.empty(Dictionary.Keys.BigUint(256),Dictionary.Values.Cell()),
+            stake_at: 0,
+            saved_validator_set_hash: 25n,
+            validator_set_changes_count: 0n,
+            validator_set_change_time: 0n,
+            stake_held_for: 10n,
+            config_proposal_votings: Dictionary.empty(Dictionary.Keys.BigUint(256),Dictionary.Values.Cell())
+        }
+    , await compile('BasicNominatorPool')));
 
     await basicNominatorPool.sendDeploy(provider.sender(), toNano('0.05'));
 
