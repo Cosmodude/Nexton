882c7689e429ff63caba40d38d38c1c6f1049db0
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-15 12:49:46 +0900

    fixed the imports for tests

--- wrappers/WhalesNominator.compile.ts
+++ wrappers/WhalesNominator.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton-community/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/whales_nominator.fc'],
+    targets: ['contracts/references/whales_nominator.fc'],
 };
