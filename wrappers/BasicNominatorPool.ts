5befd0587dc3d9920e520cde77ee4e80faaf4e29
Cosmodude <vladislav.lenskii@gmail.com>
2024-01-13 11:16:45 +0900

    updating the wrapper

--- wrappers/BasicNominatorPool.ts
+++ wrappers/BasicNominatorPool.ts
@@ -1,6 +1,27 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type BasicNominatorPoolConfig = {};
+export type InnerConfig = {
+    validator_address: Address;
+    validator_reward_share: number;
+    max_nominators_count: number;
+    min_validator_stake: number;
+    min_nominator_stake: number;
+}
+export type BasicNominatorPoolConfig = {
+    state: number;
+    nominators_count: number;
+    stake_amount_sent: number;
+    validator_amount: number;
+    config: any; //InnerConfig;
+    nominators: any; //dict
+    withdraw_requests: any; //dict
+    stake_at: number;
+    saved_validator_set_hash: bigint;
+    validator_set_changes_count: bigint;
+    validator_set_change_time: bigint;
+    stake_held_for: bigint;
+    config_proposal_votings: any; //dict
+};
 
 export function basicNominatorPoolConfigToCell(config: BasicNominatorPoolConfig): Cell {
     return beginCell().endCell();
