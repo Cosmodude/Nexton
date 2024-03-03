efd878e3037a23b0bbcb0a978c5256a9e73eb7b9
Cosmodude <vladislav.lenskii@gmail.com>
2024-03-03 11:13:25 +0900

    wrapper prepared

--- wrappers/WhalesNominator.ts
+++ wrappers/WhalesNominator.ts
@@ -1,9 +1,48 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Dictionary, Sender, SendMode } from 'ton-core';
 
-export type WhalesNominatorConfig = {};
+export type WhalesNominatorConfig = {
+    owner: Address;
+    controller: Address;
+    proxy: Address;
+    profit_per_coin: bigint;
+    nominators: Dictionary<bigint,Cell>; 
+    proxy_state: Cell;
+    min_stake: bigint;
+    deposit_fee: bigint;
+    withdraw_fee: bigint;
+    pool_fee: bigint;
+};
 
 export function whalesNominatorConfigToCell(config: WhalesNominatorConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeInt(0n,1)  // locked 
+        .storeAddress(config.owner)
+        .storeAddress(config.controller)
+        .storeAddress(config.proxy)
+        .storeRef(  // balance cell
+            beginCell()
+                .storeInt(config.profit_per_coin,128)
+                .storeCoins(0n) // balance 
+                .storeCoins(0n) // balance sent 
+                .storeCoins(0n) // blance withdraw 
+                .storeCoins(0n)
+                .storeCoins(0n)  // pending deposits
+            .endCell()
+        )
+        .storeDict(config.nominators)
+        .storeRef(config.proxy_state)
+        // .storeRef(  // extras cell
+        //     beginCell()
+        //         .storeInt(-1n,1)  // enabled
+        //         .storeInt(0n,1) // updates enabled
+        //         .storeCoins(config.min_stake)
+        //         .storeCoins(config.deposit_fee)
+        //         .storeCoins(config.withdraw_fee)
+        //         .storeCoins(config.pool_fee)
+        //         .storeCoins(0) // receipt price
+        //     .endCell()
+        // )
+    .endCell();
 }
 
 export class WhalesNominator implements Contract {
