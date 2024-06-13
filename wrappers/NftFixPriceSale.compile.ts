ef9d071b30e6ecb0aea522ca4a4e67cebdbb95af
Cosmodude <vladislav.lenskii@gmail.com>
2024-06-13 16:37:28 +0900

    fix price sale contract + wrapper

---
+++ wrappers/NftFixPriceSale.compile.ts
@@ -0,0 +1,6 @@
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+    lang: 'func',
+    targets: ['contracts/nft_marketplace/nft_fix_price_sale.fc'],
+};
